40f4e0ca4ebf5fb2c2f14b87e94b4f6517639f05
Nermin Đedović <62862650+dedovicnermin@users.noreply.github.com>
2023-07-25 21:42:16 -0500

    qc : improve logging / readability  (#4)
    * qc : bump pipeline image version
    * qc : pipeline refactor
    * qc : consumer for demoing client quotas
    * qc : avro consumer for demo testing
    * qc : refactor kustomize componentents - producer
    * qc : refactor kustomize componentents - consumer
    * qc : improve logging / readability
    * qc : test 1.2.4

--- src/main/java/io/nermdev/kafka/quota_client/clients/producer/BufferedQuotaProducerSample.java
+++ src/main/java/io/nermdev/kafka/quota_client/clients/producer/BufferedQuotaProducerSample.java
@@ -1,18 +1,20 @@
 package io.nermdev.kafka.quota_client.clients.producer;
 
 import static io.nermdev.kafka.quota_client.framework.config.ConfigUtils.getProperties;
-import static java.lang.System.out;
 
 import io.nermdev.kafka.quota_client.StatsPrinter;
 import io.nermdev.kafka.quota_client.framework.exception.ClientConfigException;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import java.util.Properties;
 
 public final class BufferedQuotaProducerSample {
+  static final Logger log = LoggerFactory.getLogger(BufferedQuotaProducerSample.class);
   public static void main(String[] args) throws ClientConfigException {
     final Properties properties = getProperties(args);
     final String topic = (String) properties.getOrDefault("topic", "volume-test");
@@ -31,7 +33,8 @@ public final class BufferedQuotaProducerSample {
 
         final var record = new ProducerRecord<>(topic, key, value);
         final var tookMs = timed(() -> producer.send(record, callback));
-        out.format("Blocked for %,d ms%n", tookMs);
+        final String format = String.format("Blocked for %,d ms", tookMs);
+        log.info(format);
         statsPrinter.maybePrintStats();
       }
     }
